fb51b8b813957c528483159e2f45cd82c5250abe
Elya Athln <elyaathlan1998@gmail.com>
2024-06-02 13:35:25 +0300

    SettingsActivity front end done

--- app/src/main/java/com/avivz_gavriels_elyaha/dailymealrecipes/SettingsActivity.java
+++ app/src/main/java/com/avivz_gavriels_elyaha/dailymealrecipes/SettingsActivity.java
@@ -1,12 +1,16 @@
 package com.avivz_gavriels_elyaha.dailymealrecipes;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.Switch;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -16,7 +20,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SettingsActivity extends AppCompatActivity {
-    private String[] recentHistoryOptions = {"5", "10", "15", "All"};
+    private final String[] recentHistoryOptions = {"5", "10", "15", "All"};
+    private final String[] recipeCategories = {"Breakfast", "Lunch", "Dinner", "Dessert"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +33,12 @@ public class SettingsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        SettingsActivity that = this;
+        // recent recipe history counter
         AutoCompleteTextView recentHistoryAdapter = findViewById(R.id.recentHistoryDropDown);
         recentHistoryAdapter.setText(this.recentHistoryOptions[0]);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, recentHistoryOptions);
-        recentHistoryAdapter.setAdapter(adapter);
+        ArrayAdapter<String> adapterHistory = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, recentHistoryOptions);
+        recentHistoryAdapter.setAdapter(adapterHistory);
         recentHistoryAdapter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -40,6 +47,19 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        // recipe categories
+        AutoCompleteTextView categoryAdapter = findViewById(R.id.categoryDropDown);
+        categoryAdapter.setText(this.recipeCategories[0]);
+        ArrayAdapter<String> adapterCategory = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, recipeCategories);
+        categoryAdapter.setAdapter(adapterCategory);
+        categoryAdapter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(SettingsActivity.this, "Selected: " + item, Toast.LENGTH_SHORT).show();
+            }
+        });
+        // enable notification switch
         Switch notificationSwitch = findViewById(R.id.enableNotificationSwitch);
         notificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -48,5 +68,47 @@ public class SettingsActivity extends AppCompatActivity {
                 Toast.makeText(SettingsActivity.this, "Notification state: " + state, Toast.LENGTH_SHORT).show();
             }
         });
+        // enable kosher switch
+        Switch kosherSwitch = findViewById(R.id.kosherSwitch);
+        kosherSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                boolean state = buttonView.isChecked();
+                Toast.makeText(SettingsActivity.this, "Notification state: " + state, Toast.LENGTH_SHORT).show();
+            }
+        });
+        // enable quick switch
+        Switch quickSwitch = findViewById(R.id.quickSwitch);
+        quickSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                boolean state = buttonView.isChecked();
+                Toast.makeText(SettingsActivity.this, "Notification state: " + state, Toast.LENGTH_SHORT).show();
+            }
+        });
+        // enable low calories switch
+        Switch lowCaloriesSwitch = findViewById(R.id.lowCaloriesSwitch);
+        lowCaloriesSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                boolean state = buttonView.isChecked();
+                Toast.makeText(SettingsActivity.this, "Notification state: " + state, Toast.LENGTH_SHORT).show();
+            }
+        });
+        ImageButton timePicker = findViewById(R.id.timePicker);
+        timePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("at onClick", "my log");
+                TimePickerDialog clockPicker = new TimePickerDialog(that, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Toast.makeText(that, hourOfDay + ":" + minute, Toast.LENGTH_SHORT).show();
+                    }
+                }, 0, 0, true);
+                clockPicker.show();
+            }
+        });
     }
 }
+
