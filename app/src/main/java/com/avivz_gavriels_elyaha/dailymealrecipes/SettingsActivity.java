312edbaeecf2be91af56a1bce730890be86d07f7
Elya Athln <elyaathlan1998@gmail.com>
2024-06-02 14:36:28 +0300

    added shared preferences

--- app/src/main/java/com/avivz_gavriels_elyaha/dailymealrecipes/SettingsActivity.java
+++ app/src/main/java/com/avivz_gavriels_elyaha/dailymealrecipes/SettingsActivity.java
@@ -1,101 +1,115 @@
 package com.avivz_gavriels_elyaha.dailymealrecipes;
 
 import android.app.TimePickerDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.TimePicker;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SettingsActivity extends AppCompatActivity {
+    private SharedPreferences sp;
     private final String[] recentHistoryOptions = {"5", "10", "15", "All"};
     private final String[] recipeCategories = {"Breakfast", "Lunch", "Dinner", "Dessert"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
         SettingsActivity that = this;
+
+        sp = getSharedPreferences("settings", MODE_PRIVATE);
+
         // recent recipe history counter
         AutoCompleteTextView recentHistoryAdapter = findViewById(R.id.recentHistoryDropDown);
-        recentHistoryAdapter.setText(this.recentHistoryOptions[0]);
+        recentHistoryAdapter.setText(sp.getString("recentHistory", "5"));
         ArrayAdapter<String> adapterHistory = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, recentHistoryOptions);
         recentHistoryAdapter.setAdapter(adapterHistory);
         recentHistoryAdapter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String item = parent.getItemAtPosition(position).toString();
-                Toast.makeText(SettingsActivity.this, "Selected: " + item, Toast.LENGTH_SHORT).show();
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("recentHistory", item);
+                editor.apply();
             }
         });
 
         // recipe categories
         AutoCompleteTextView categoryAdapter = findViewById(R.id.categoryDropDown);
-        categoryAdapter.setText(this.recipeCategories[0]);
+        categoryAdapter.setText(sp.getString("category", "Breakfast"));
         ArrayAdapter<String> adapterCategory = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, recipeCategories);
         categoryAdapter.setAdapter(adapterCategory);
         categoryAdapter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String item = parent.getItemAtPosition(position).toString();
-                Toast.makeText(SettingsActivity.this, "Selected: " + item, Toast.LENGTH_SHORT).show();
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("category", item);
+                editor.apply();
+
             }
         });
         // enable notification switch
         Switch notificationSwitch = findViewById(R.id.enableNotificationSwitch);
+        notificationSwitch.setChecked(sp.getBoolean("notification", false));
         notificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 boolean state = buttonView.isChecked();
-                Toast.makeText(SettingsActivity.this, "Notification state: " + state, Toast.LENGTH_SHORT).show();
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putBoolean("notification", state);
+                editor.apply();
             }
         });
         // enable kosher switch
         Switch kosherSwitch = findViewById(R.id.kosherSwitch);
+        kosherSwitch.setChecked(sp.getBoolean("kosher", true));
         kosherSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 boolean state = buttonView.isChecked();
-                Toast.makeText(SettingsActivity.this, "Notification state: " + state, Toast.LENGTH_SHORT).show();
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putBoolean("kosher", state);
+                editor.apply();
             }
         });
         // enable quick switch
         Switch quickSwitch = findViewById(R.id.quickSwitch);
+        quickSwitch.setChecked(sp.getBoolean("quick", false));
         quickSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 boolean state = buttonView.isChecked();
-                Toast.makeText(SettingsActivity.this, "Notification state: " + state, Toast.LENGTH_SHORT).show();
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putBoolean("quick", state);
+                editor.apply();
             }
         });
         // enable low calories switch
         Switch lowCaloriesSwitch = findViewById(R.id.lowCaloriesSwitch);
+        lowCaloriesSwitch.setChecked(sp.getBoolean("lowCalories", false));
         lowCaloriesSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 boolean state = buttonView.isChecked();
-                Toast.makeText(SettingsActivity.this, "Notification state: " + state, Toast.LENGTH_SHORT).show();
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putBoolean("lowCalories", state);
+                editor.apply();
             }
         });
-        ImageButton timePicker = findViewById(R.id.timePicker);
+        Button timePicker = findViewById(R.id.timePicker);
+        timePicker.setText(sp.getInt("timerHour", 0) + ":" + sp.getInt("timerMinute", 0));
+
         timePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -103,7 +117,14 @@ public class SettingsActivity extends AppCompatActivity {
                 TimePickerDialog clockPicker = new TimePickerDialog(that, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        Toast.makeText(that, hourOfDay + ":" + minute, Toast.LENGTH_SHORT).show();
+                        SharedPreferences.Editor editor = sp.edit();
+
+
+                        editor.putInt("timerHour", hourOfDay);
+                        editor.putInt("timerMinute", minute);
+                        editor.apply();
+
+                        timePicker.setText(sp.getInt("timerHour", 0) + ":" + sp.getInt("timerMinute", 0));
                     }
                 }, 0, 0, true);
                 clockPicker.show();
