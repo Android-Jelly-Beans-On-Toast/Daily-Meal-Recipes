e065a1b9db7c5a424f1a1c8d0dad823878702c59
Elya Athln <elyaathlan1998@gmail.com>
2024-06-02 11:54:34 +0300

    SettingsActivity was updated

--- app/src/main/java/com/avivz_gavriels_elyaha/dailymealrecipes/SettingsActivity.java
+++ app/src/main/java/com/avivz_gavriels_elyaha/dailymealrecipes/SettingsActivity.java
@@ -1,18 +1,19 @@
 package com.avivz_gavriels_elyaha.dailymealrecipes;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AutoCompleteTextView;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
     private String[] recentHistoryOptions = {"5", "10", "15", "All"};
@@ -37,6 +38,15 @@ public class SettingsActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String item = parent.getItemAtPosition(position).toString();
                 Toast.makeText(SettingsActivity.this, "Selected: " + item, Toast.LENGTH_SHORT).show();
+            }
+        });
+        Switch notificationSwitch;
+        notificationSwitch = findViewById(R.id.enableNotificationSwitch);
+        notificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                boolean state = buttonView.isChecked();
+                Toast.makeText(SettingsActivity.this, "Notification state: " + state, Toast.LENGTH_SHORT).show();
             }
         });
     }
