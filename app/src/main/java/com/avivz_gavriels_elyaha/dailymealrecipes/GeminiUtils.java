898d6aec594b81c81a2da486c81face5dbd596cf
Elya Athln <elyaathlan1998@gmail.com>
2024-06-16 11:45:36 +0300

    added camera open and remove warnings

---
+++ app/src/main/java/com/avivz_gavriels_elyaha/dailymealrecipes/GeminiUtils.java
@@ -0,0 +1,11 @@
+package com.avivz_gavriels_elyaha.dailymealrecipes;
+
+import com.google.ai.client.generativeai.GenerativeModel;
+
+public class GeminiUtils {
+    GenerativeModel geminiModel;
+
+    public GeminiUtils(String apiKey) {
+        geminiModel = new GenerativeModel(apiKey);
+    }
+}
