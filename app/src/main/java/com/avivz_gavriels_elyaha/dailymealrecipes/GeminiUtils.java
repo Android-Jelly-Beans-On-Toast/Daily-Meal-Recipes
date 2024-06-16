f483dcda86778b7297a84509286526ed582da42e
Elya Athln <elyaathlan1998@gmail.com>
2024-06-16 14:26:47 +0300

    trust me i am engeneer

--- app/src/main/java/com/avivz_gavriels_elyaha/dailymealrecipes/GeminiUtils.java
+++ app/src/main/java/com/avivz_gavriels_elyaha/dailymealrecipes/GeminiUtils.java
@@ -1,13 +1,12 @@
 package com.avivz_gavriels_elyaha.dailymealrecipes;
 
 
-import com.google.ai.client.generativeai.BuildConfig;
 import com.google.ai.client.generativeai.GenerativeModel;
 
 public class GeminiUtils {
     GenerativeModel geminiModel;
 
     public GeminiUtils(String apiKey) {
-        geminiModel = new GenerativeModel("gemini", BuildConfig.apiKey);
+        geminiModel = new GenerativeModel("gemini", "");
     }
 }
