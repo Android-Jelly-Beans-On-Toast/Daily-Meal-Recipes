f4a0959931fc5fa9f23843bb77cc7ba5d4d1e39c
aviv0404 <aviv040400@gmail.com>
2024-06-19 19:08:00 +0300

    added RecipeActivity activity

---
+++ app/src/main/java/com/avivz_gavriels_elyaha/dailymealrecipes/RecipeActivity.java
@@ -0,0 +1,19 @@
+package com.avivz_gavriels_elyaha.dailymealrecipes;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+
+    }
+}
